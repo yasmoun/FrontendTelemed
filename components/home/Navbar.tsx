fe65b4540441bd96aa4064364d823902cd911cd7
yasmine <138599652+yasmoun@users.noreply.github.com>
2025-02-25 02:38:04 +0100

    nav

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -26,7 +26,7 @@ function Navbar() {
         </ul>
         <div className="flex space-x-4">
           <button className="button bg-white rounded-lg w-150 text-black">Login</button>
-          <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#1F363D,45%,#000,55%,#1F363D)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">Register</button>
+          <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#1F363D,45%,#000,55%,#1F363D)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors hover:outline-none focus:ring-2 hover:ring-slate-400 hover:ring-offset-2 hover:ring-offset-slate-50">Register</button>
         </div>
       </div>
       <div className="bg-blue-custom text-blue-custom h-2"></div>
