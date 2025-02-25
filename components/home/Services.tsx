fda25eeef32d69065ed1cd8ef7eb6fac77beefd7
yasmine <138599652+yasmoun@users.noreply.github.com>
2025-02-25 02:28:43 +0100

    nav
    nav

--- components/home/Services.tsx
+++ components/home/Services.tsx
@@ -1,86 +1,37 @@
-import React from 'react';
+"use client"
 import Image from 'next/image';
+import Link from "next/link";
 
-const services = [
-  {
-    title: "Virtual Consultations",
-    image: "/home/virtual-consultations.jpeg",
-    description: "Our platform connects you directly with trusted doctors through secure video calls. Discuss your health concerns from the comfort of your home, ensuring convenience and prompt medical advice without the need for in-person visits.",
-  },
-  {
-    title: "Worldwide Telemedicine",
-    image: "/home/worldwide-telemedecine.png",
-    description: "Our teleconsultation app empowers users worldwide to access quality healthcare anytime, anywhere.",
-  },
-  {
-    title: "Smart Medication Search",
-    image: "/home/smart-medecation-search.png",
-    description: "Easily find information about any medication by simply uploading a photo.",
-  },
-  {
-    title: "24/7 Availability",
-    image: "/home/availability.png",
-    description: "Access medical consultations anytime, day or night. Our healthcare professionals are ready to assist you round-the-clock through our secure platform, ensuring you never have to wait for essential medical advice.",
-  },
-  {
-    title: "Multiple Specialties",
-    image: "/home/mutiple-specialities.png",
-    description: "Our platform hosts a wide range of medical professionals, including specialists. Whatever your health concern, find the right healthcare provider with just a few clicks.",
-  },
-  {
-    title: "Digital Prescriptions",
-    image: "/home/digital-prescreptions.png",
-    description: "Skip the paperwork - get your prescriptions sent directly through our platform after your consultation. Safe, quick, and hassle-free. Keep track of all your prescriptions in one place.",
-  },
-  {
-    title: "AI Health Assistant",
-    image: "/home/ai-health-assistant.png",
-    description: "Meet our friendly medical chatbot, available 24/7 to guide you through your health questions. Quick, reliable answers and initial health assessments at your fingertips.",
-  },
-  {
-    title: "Pharmacy Locator",
-    image: "/home/pharmacy-locator.png",
-    description: "Find the nearest pharmacy in our network with our easy-to-use locator. Find a convenient pharmacy near you and collect your medications when it suits you.",
-  },
-  {
-    title: "Anonymous Medical Q&A Platform",
-    image: "/home/digital-prescreptions.png",
-    description: "Skip the paperwork - get your prescriptions sent directly through our platform after your consultation. Safe, quick, and hassle-free. Keep track of all your prescriptions in one place.",
-  }
-];
 
-const Services = () => {
+function Navbar() {
   return (
-    <div className="mx-auto py-12 px-4 sm:px-6 lg:px-8 bg-white">
-      <h2 className="text-3xl font-medium text-gray-700 mb-12 text-center">
-        Our Services
-      </h2>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {services.map((service, index) => (
-          <div key={index} className="border border-gray-200 rounded-lg p-6 flex flex-col items-center">
-            <div className="mb-6">
-              <Image
-                src={service.image}
-                alt={service.title}
-                width={160}
-                height={160}
-                className="rounded-lg"
-              />
-            </div>
-
-            <h3 className="text-xl font-medium text-gray-800 text-center mb-3">
-              {service.title}
-            </h3>
-
-            <p className="text-gray-600 text-center">
-              {service.description}
-            </p>
-          </div>
-        ))}
+    <nav className="navbar w-full">
+      <div className="max-width flex justify-between items-center py-4">
+        <div className="flex items-center">
+          <Image src="/logo.png" alt="Logo" width={140} height={58} className="block" />
+        </div>
+        <ul className="flex space-x-4 items-center font-semibold">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/">About Us</Link>
+          </li>
+          <li>
+            <Link href="/">Services</Link>
+          </li>
+          <li>
+            <Link href="/">Contact</Link>
+          </li>
+        </ul>
+        <div className="flex space-x-4">
+          <button className="button bg-white rounded-lg w-150 text-black">Login</button>
+          <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#1F363D,45%,#000,55%,#1F363D)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">Register</button>
+        </div>
       </div>
-    </div>
+      <div className="bg-blue-custom text-blue-custom h-2"></div>
+    </nav>
   );
-};
+}
 
-export default Services;
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,51 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+ 
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shimmer: "shimmer 2s linear infinite"
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0"
+          },
+          to: {
+            backgroundPosition: "-200% 0"
+          }
+        }
+      },
+      colors: {
+        'green-custom': '#4D7452',
+        'blue-custom': '#1F363D',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 }
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
 
