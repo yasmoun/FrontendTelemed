1e446f98d22d115502b4be6aa3dce2c91663aebf
yasmine <138599652+yasmoun@users.noreply.github.com>
2025-02-25 02:35:54 +0100

    chang

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+"use client"
 import Image from 'next/image';
 import Link from "next/link";
 
-const Navbar: React.FC = () => {
+
+function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="max-width flex-between padding-x padding-y">
-        <div className="flex-center">
-          <Image src="/logo.png" alt="Logo" width={110} height={150} />
+    <nav className="navbar w-full">
+      <div className="max-width flex justify-between items-center py-4">
+        <div className="flex items-center">
+          <Image src="/logo.png" alt="Logo" width={140} height={58} className="block" />
         </div>
-<ul>
+        <ul className="flex space-x-4 items-center font-semibold">
           <li>
             <Link href="/">Home</Link>
           </li>
@@ -23,12 +24,12 @@ const Navbar: React.FC = () => {
             <Link href="/">Contact</Link>
           </li>
         </ul>
-        <div className="flex-center padding-x-40">
-        <button className="button bg-white rounded-lg w-150 text-black">Login</button>
-        <button className="button bg-black rounded-lg">Register</button>
+        <div className="flex space-x-4">
+          <button className="button bg-white rounded-lg w-150 text-black">Login</button>
+          <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#1F363D,45%,#000,55%,#1F363D)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">Register</button>
+        </div>
       </div>
-      </div>
-      
+      <div className="bg-blue-custom text-blue-custom h-2"></div>
     </nav>
   );
 }
